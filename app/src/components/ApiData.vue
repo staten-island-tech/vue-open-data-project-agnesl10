a860ef7e202f68ea042566024a441a8230c15a06
agnesl10 <agnesl10@nycstudents.net>
2025-03-06 11:48:47 -0500

    sdfsdf

---
+++ app/src/components/ApiData.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+const api = `https://data.cityofnewyork.us/resource/jb7j-dtam.json`;
+async function (data) {}
+</script>
+
+<style scoped></style>
