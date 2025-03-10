fd758332ba0d581842d9cceaaac6d7ae4f8a82fd
agnesl10 <agnesl10@nycstudents.net>
2025-03-10 12:32:12 -0400

    sdfsdf

--- app/src/components/CausesChart.vue
+++ app/src/components/CausesChart.vue
@@ -4,6 +4,18 @@
 
 <script setup>
 import { Bar } from 'vue-chart.js'
+import apiData from '@/components/ApiData'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 </script>
 
 <style scoped></style>
