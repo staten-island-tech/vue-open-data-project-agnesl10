43551f7aefb876be651b7c57abc5bb890076ef2d
agnesl10 <agnesl10@nycstudents.net>
2025-03-10 12:19:49 -0400

    sdfsdf

--- app/src/components/ApiData.vue
+++ app/src/components/ApiData.vue
@@ -3,8 +3,23 @@
 </template>
 
 <script setup>
-const api = `https://data.cityofnewyork.us/resource/jb7j-dtam.json`;
-async function (data) {}
+const api = `https://data.cityofnewyork.us/resource/jb7j-dtam.json`
+async function getCauses() {
+  try {
+    //fetch returns a promise
+    const response = await fetch(api)
+    //gaurd clause
+    if (response.status != 200) {
+      throw new Error(response)
+    } else {
+      const data = await response.json()
+      console.log(data)
+    }
+  } catch (error) {
+    console.log(error)
+    alert('sorry, could not find the data')
+  }
+}
 </script>
 
 <style scoped></style>
