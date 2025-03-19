b3bd6c27630091d765e21d72b12eb92ca1a54969
agnesl10 <agnesl10@nycstudents.net>
2025-03-19 12:32:16 -0400

    sdfsdf

--- app/src/components/CardProps.vue
+++ app/src/components/CardProps.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="card">
-    <h1>{{ info.year }}</h1>
-    <h2>{{ info.leading_cause }}</h2>
-    <h2>{{ info.sex }}</h2>
+  <div
+    class="card w-3xs h-3xs rounded-2xl bg-stone-400 items-center text-center m-3 justify-between flex flex-col p-3"
+  >
+    <h1 class="text-xl">{{ info.year }}</h1>
+    <h2>Cause : {{ info.leading_cause }}</h2>
+    <h3>Race : {{ info.race_ethnicity }}</h3>
+    <h3>Gender : {{ info.sex }}</h3>
   </div>
 </template>
 
--- app/src/views/ShowData.vue
+++ app/src/views/ShowData.vue
@@ -1,17 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-wrap justify-around flex-row">
     <ApiData />
   </div>
 </template>
 
 <script setup>
-import CardProps from '@/components/CardProps.vue'
 import ApiData from '@/components/ApiData.vue'
-const info = {
-  year: 1999,
-  leading_cause: 'Poop',
-  sex: 'Female',
-}
 </script>
 
 <style scoped></style>
